97459f495d478ba5040ed5408fadc10486e33f52
pwfreedm <pwfreedm@github.com>
2024-05-27 15:41:49 -0400

    starting encryption, ooh boy

---
+++ src/encryption.rs
@@ -0,0 +1,6 @@
+
+//probably just return a matrix here? maybe return coordinate pairs as a stepping stone
+// pub fn encrypt (raw_data: &mut String, private_keys: &Vec<i8>) -> Vec<u32>
+// {
+
+// }
